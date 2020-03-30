3b7fafe7c5e1731d83afdf1eee937d13b7a0c732
Master52 <harajmast9008@gmail.com>
2020-03-30 07:58:08 +0530

    Fixed plot layout

--- src/components/Plot.js
+++ src/components/Plot.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../../node_modules/react-vis/dist/style.css';
+import DiscreteColorLegend from '../../node_modules/react-vis/dist/legends/discrete-color-legend';
 import {XYPlot,LineSeries} from 'react-vis';
 import {HorizontalGridLines,VerticalGridLines} from 'react-vis';
 import {XAxis,YAxis} from 'react-vis';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box'
 import Divider from '@material-ui/core/Divider';
+import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 function getPlottingData(history)
 {
@@ -20,9 +23,22 @@ function getPlottingData(history)
     return data
 }
 
-const Plot = (props) => {
-    console.log(props.overall);
+const useStyle = makeStyles((theme) => ({
+    content: {
+        margin: theme.spacing(7),
+    },
+    graph: {
+        marginLeft: theme.spacing(15),
+        marginTop: theme.spacing(3),
+    }
+}));
 
+const Plot = (props) => {
+    const classes = useStyle();
+     var items =  [
+        {title: 'Deaths', color: 'red'},
+        {title: 'Cases', color: 'blue'},
+      ]
     /*
      * cases: 105726
         todayCases: 1600
@@ -39,52 +55,69 @@ const Plot = (props) => {
     return(
         <div >
             <Box display = "flex" flexDirection = "row">
-                <Box display = "flex" flexDirection = "column" >
-                    <Typography color  = "textSecondary" variant = "h6" >
+                <Box className = {classes.content} display = "flex" flexDirection = "column" >
+                    <Typography color  = "textSecondary" variant = "body1" >
                         Total Cases: {props.overall.cases}
 
                     </Typography>
                     <Divider variant = "middel" />
 
-                    <Typography color  = "primary" variant = "h6" >
+                    <Typography color  = "primary" variant = "body1" >
                         Today Cases: {props.overall.todayCases}
                     </Typography>
                     <Divider variant = "middel" />
 
-                    <Typography color  = "secondary" variant = "h6" >
+                    <Typography color  = "secondary" variant = "body1" >
                          Total Death: {props.overall.deaths}
                     </Typography>
                     <Divider variant = "middel" />
 
-                    <Typography color  = "secondary" variant = "h6" >
+                    <Typography color  = "secondary" variant = "body1" >
                         Today Death: {props.overall.todayDeaths}
                     </Typography>
                     <Divider variant = "middel" />
 
-                    <Typography color  = "success" variant = "h6" >
+                    <Typography color  = "success" variant = "body1" >
                         Recovered:  {props.overall.recovered}
                     </Typography>
                     <Divider variant = "middel" />
 
-                    <Typography color  = "textSPrimary" variant = "h6" >
+                    <Typography color  = "textSPrimary" variant = "body1" >
                         Active:  {props.overall.recovered}
                     </Typography>
                     <Divider variant = "middel" />
 
-                    <Typography color  = "error" variant = "h6" >
+                    <Typography color  = "error" variant = "body1" >
                         Critical:  {props.overall.critical}
                     </Typography>
                     <Divider variant = "middel" />
-
-
+                    <br />
+                    <br />
+                    <Button color = 'primary' variant = 'contained' >
+                        Back
+                    </Button>
                 </Box>
-                <Box display = "flex" >
+                <Box display = "flex" className = {classes.graph} >
                     <XYPlot xType="time" height = {500} width = {600} >
                         <XAxis tickLabelAngle={-45} />
                         <YAxis tickLabelAngle={-90} />
+                        <VerticalGridLines />
+                        <HorizontalGridLines />
                         <LineSeries
-                          data={getPlottingData(props.history.timeline.cases)} />
+                        data={getPlottingData(props.history.timeline.deaths)}
+                        strokeWidth = {5}
+                        color = 'red'
+                        />
+                        <LineSeries
+                        data={getPlottingData(props.history.timeline.cases)}
+                        strokeWidth = {5}
+                        />
                     </XYPlot>
+                    <DiscreteColorLegend
+                      height={100}
+                      width={100}
+                      items={items}
+                    />
                 </Box>
             </Box>
         </div>
